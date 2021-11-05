06fb21594dbdc41acb1e0a2ce21a25879749b1b2
Daniel Saul <daniel@dansaul.co.uk>
2021-11-05 20:00:12 +0000

    No content

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -14,11 +14,11 @@ const { page } = useData()
       <ToggleDarkMode class="fixed m-8 top-0 right-0 sm:inset-auto sm:bottom-0 sm:left-0 z-10" />
     </div>
     <div class="p-8 pt-12">
-      <div class="mb-8">
-        <h1 class="text-3xl font-bold pb-2">{{ page.title }}</h1>
-        <p class="text-xs italic font-serif">{{ page.frontmatter.date }}</p>
-      </div>
-      <Content class="prose prose-sm dark:prose-light" />
+      <!-- <div class="mb-8"> -->
+      <!--   <h1 class="text-3xl font-bold pb-2">{{ page.title }}</h1> -->
+      <!--   <p class="text-xs italic font-serif">{{ page.frontmatter.date }}</p> -->
+      <!-- </div> -->
+      <!-- <Content class="prose prose-sm dark:prose-light" /> -->
     </div>
   </div>
 </template>
